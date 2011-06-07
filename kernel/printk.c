ef74add21cdaf6a9c10e50d4eed2315b7fd19878
Peter Zijlstra <a.p.zijlstra@chello.nl>
2011-06-07 11:17:30 +0200

    printk, lockdep: Disable lock debugging on zap_locks()
    zap_locks() is used by printk() in a last ditch effort to get data
    out, clearly we cannot trust lock state after this so make it disable
    lock debugging.
    
    Also don't treat printk recursion through lockdep as a normal
    recursion bug but try hard to get the lockdep splat out.
    
    Change-Id: Ib6ed60a37e194f29a37844851b100ac99fbb61db
    Signed-off-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Link: http://lkml.kernel.org/n/tip-kqxwmo4xz37e1s8w0xopvr0q@git.kernel.org
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/printk.c
+++ kernel/printk.c
@@ -756,6 +756,7 @@ static void zap_locks(void)
 
 	oops_timestamp = jiffies;
 
+	debug_locks_off();
 	/* If a crash is occurring, make sure we can't deadlock */
 	raw_spin_lock_init(&logbuf_lock);
 	/* And make sure that we print immediately */
@@ -924,7 +925,7 @@ asmlinkage int vprintk(const char *fmt, va_list args)
 		 * recursion and return - but flag the recursion so that
 		 * it can be printed at the next appropriate moment:
 		 */
-		if (!oops_in_progress) {
+		if (!oops_in_progress && !lockdep_recursing(current)) {
 			recursion_bug = 1;
 			goto out_restore_irqs;
 		}
