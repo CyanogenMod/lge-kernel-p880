e0cf272a60925e8716925d7a0ffdeb6832b6fe31
Al Viro <viro@zeniv.linux.org.uk>
2011-07-26 01:52:52 -0400

    switch ->mknod() to umode_t
    Change-Id: Ib75438537c389a540807c6040ab1abb1d4822826
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/ext3/namei.c
+++ fs/ext3/namei.c
@@ -1729,7 +1729,7 @@ retry:
 }
 
 static int ext3_mknod (struct inode * dir, struct dentry *dentry,
-			int mode, dev_t rdev)
+			umode_t mode, dev_t rdev)
 {
 	handle_t *handle;
 	struct inode *inode;
