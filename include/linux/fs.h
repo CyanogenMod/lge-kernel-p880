5e7c4e81fd176b8b92d06c7afe0ae3326f6cfb05
Konstantin Khlebnikov <khlebnikov@openvz.org>
2011-12-08 14:33:54 -0800

    vmscan: use atomic-long for shrinker batching
    Use atomic-long operations instead of looping around cmpxchg().
    
    [akpm@linux-foundation.org: massage atomic.h inclusions]
    Signed-off-by: Konstantin Khlebnikov <khlebnikov@openvz.org>
    Cc: Dave Chinner <david@fromorbit.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Change-Id: I8f1ed638730c005c8fa77aed760315d720f4c4d1

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -392,8 +392,8 @@ struct inodes_stat_t {
 #include <linux/semaphore.h>
 #include <linux/fiemap.h>
 #include <linux/rculist_bl.h>
-#include <linux/shrinker.h>
 #include <linux/atomic.h>
+#include <linux/shrinker.h>
 
 #include <asm/byteorder.h>
 
