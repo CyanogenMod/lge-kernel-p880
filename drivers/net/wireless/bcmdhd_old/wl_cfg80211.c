d05cc82f62e5c0e5057e9abcca2c0167f601cc16
Dmitry Shmidt <dimitrysh@google.com>
2012-06-28 10:25:25 -0700

    net: wireless: bcmdhd: Skip inaccurate wl_construct_reginfo() call
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- drivers/net/wireless/bcmdhd_old/wl_cfg80211.c
+++ drivers/net/wireless/bcmdhd_old/wl_cfg80211.c
@@ -7215,11 +7215,13 @@ s32 wl_update_wiphybands(struct wl_priv *wl)
 		}
 	}
 
+#if 0
 	err = wl_construct_reginfo(wl, bw_cap);
 	if (err) {
 		WL_ERR(("wl_construct_reginfo() fails err=%d\n", err));
 		return err;
 	}
+#endif
 	for (i = 1; i <= nband && i < sizeof(bandlist)/sizeof(u32); i++) {
 		index = -1;
 		if (bandlist[i] == WLC_BAND_5G && __wl_band_5ghz_a.n_channels > 0) {
