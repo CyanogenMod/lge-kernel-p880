f37d4b43e9f784a18499fdc0c47b27136ec70515
ks.kwon <ks.kwon@lge.com>
2012-08-07 13:50:18 +0900

    X3:touch:reset_pin control during touch power control. Commit taken from mako kernel source by iodak
    This commit is to assert/de-assert reset signal during suspend/resume.
    Without this commit, there is a 2~3mA of leakage current from reset pin
    to touch contorller IC.
    
    Change-Id: I32717412b1dec13996aa986c7939c76dd6453ebb

--- drivers/input/touchscreen/lge_touch_core.c
+++ drivers/input/touchscreen/lge_touch_core.c
@@ -754,11 +754,10 @@ static int accuracy_filter_func(struct lge_touch_data *ts)
 
 	// finish the accuracy_filter
 	if(ts->accuracy_filter.finish_filter == 1 && 
-	   (ts->accuracy_filter.his_data.count > ts->accuracy_filter.touch_max_count
-	    || ts->ts_data.total_num != 1
-	    || ts->ts_data.curr_data[0].id != 0)){
-		ts->accuracy_filter.finish_filter = 0;
-		ts->accuracy_filter.his_data.count = 0;
+		(ts->accuracy_filter.his_data.count > ts->accuracy_filter.touch_max_count
+		|| ts->ts_data.total_num != 1 || ts->ts_data.curr_data[0].id != 0)){
+			ts->accuracy_filter.finish_filter = 0;
+			ts->accuracy_filter.his_data.count = 0;
 	}
 
 	// main algorithm
@@ -2730,7 +2729,7 @@ static int touch_probe(struct i2c_client *client, const struct i2c_device_id *id
 			TOUCH_ERR_MSG("FAIL: touch_reset gpio_request\n");
 			goto err_assign_platform_data;
 		}
-		gpio_direction_output(ts->pdata->reset_pin, 1);
+		gpio_direction_output(ts->pdata->reset_pin, 0);
 	}
 
 	atomic_set(&ts->device_init, 0);
