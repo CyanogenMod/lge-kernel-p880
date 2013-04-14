13e734fc51311a5dc8e1d33a2e3f075b6bfdba40
maxl <maxl@4b8ad20e-30ca-41b1-81b8-3c8b9e269e15>
2013-04-14 09:57:09 +0000

    block,elevator: use new hashtable implementation

--- include/linux/elevator.h
+++ include/linux/elevator.h
@@ -2,6 +2,7 @@
 #define _LINUX_ELEVATOR_H
 
 #include <linux/percpu.h>
+#include <linux/hashtable.h>
 
 #ifdef CONFIG_BLOCK
 
@@ -80,6 +81,8 @@ struct elevator_type
 	struct module *elevator_owner;
 };
 
+#define ELV_HASH_BITS 6
+
 /*
  * each queue has an elevator_queue associated with it
  */
@@ -90,8 +93,8 @@ struct elevator_queue
 	struct kobject kobj;
 	struct elevator_type *elevator_type;
 	struct mutex sysfs_lock;
-	struct hlist_head *hash;
 	unsigned int registered:1;
+	DECLARE_HASHTABLE(hash, ELV_HASH_BITS);
 };
 
 /*
