412cbdb5f9cf215fe4926f0294a8a32d95ec9b5a
laufersteppenwolf <laufersteppenwolf@gmail.com>
2013-04-28 14:14:54 +0200

    interactive tweaks
    Change-Id: I2422033782f283ea4675c832258eb1afb61f83de

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -78,7 +78,7 @@ static struct kobject *interactive_kobj;
 static unsigned int active_count;
 
 /* Go to max speed when CPU load at or above this value. */
-#define DEFAULT_GO_MAXSPEED_LOAD 85
+#define DEFAULT_GO_MAXSPEED_LOAD 65
 static unsigned long go_maxspeed_load;
 
 /* Base of exponential raise to max speed; if 0 - jump to maximum */
@@ -99,7 +99,7 @@ static unsigned long sustain_load;
 /*
  * The minimum amount of time to spend at a frequency before we can ramp down.
  */
-#define DEFAULT_MIN_SAMPLE_TIME 30000;
+#define DEFAULT_MIN_SAMPLE_TIME 20000;
 static unsigned long min_sample_time;
 
 /*
