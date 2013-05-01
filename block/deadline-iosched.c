27bc3c38f982ff1b7741511cde043d7d93e30063
laufersteppenwolf <laufersteppenwolf@gmail.com>
2013-05-01 22:46:42 +0200

    added I/O schedulers
    Change-Id: Ib40abd990ee872f6e2d7b0323bb7f7141430b816

--- block/deadline-iosched.c
+++ block/deadline-iosched.c
@@ -17,11 +17,11 @@
 /*
  * See Documentation/block/deadline-iosched.txt
  */
-static const int read_expire = HZ / 2;  /* max time before a read is submitted. */
-static const int write_expire = 5 * HZ; /* ditto for writes, these limits are SOFT! */
-static const int writes_starved = 2;    /* max times reads can starve a write */
-static const int fifo_batch = 16;       /* # of sequential requests treated as one
-				     by the above parameters. For throughput. */
+static const int read_expire = HZ / 4;  /* max time before a read is submitted. */
+static const int write_expire = HZ; /* ditto for writes, these limits are SOFT! */
+static const int writes_starved = 1; /* max times reads can starve a write */
+static const int fifo_batch = 1; /* # of sequential requests treated as one
+by the above parameters. For throughput. */
 
 struct deadline_data {
 	/*
