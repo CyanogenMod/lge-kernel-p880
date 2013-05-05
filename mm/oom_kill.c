3e0c7f0da356178eeb1b72395f10793b7f5bd7df
laufersteppenwolf <laufersteppenwolf@gmail.com>
2013-05-05 16:23:48 +0200

    several tweaks and GPU OC to 564 (ustable)

--- mm/oom_kill.c
+++ mm/oom_kill.c
@@ -34,7 +34,7 @@
 #include <linux/ptrace.h>
 
 int sysctl_panic_on_oom;
-int sysctl_oom_kill_allocating_task;
+int sysctl_oom_kill_allocating_task = 1;
 int sysctl_oom_dump_tasks = 1;
 static DEFINE_SPINLOCK(zone_scan_lock);
 
