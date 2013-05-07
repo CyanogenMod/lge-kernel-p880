be3aed408d11b97800a11f1ca06df2be714edd9d
laufersteppenwolf <laufersteppenwolf@gmail.com>
2013-05-07 21:10:16 +0200

    rolling back all CM merges

--- drivers/misc/tspdrv/ImmVibeSPI.c
+++ drivers/misc/tspdrv/ImmVibeSPI.c
@@ -115,7 +115,7 @@ IMMVIBESPIAPI VibeStatus SYS_API__I2C__Write( _addr, _data)
 /*
 ** Called to disable amp (disable output force)
 */
-/*IMMVIBESPIAPI*/ VibeStatus ImmVibeSPI_ForceOut_AmpDisable(VibeUInt8 nActuatorIndex)
+IMMVIBESPIAPI VibeStatus ImmVibeSPI_ForceOut_AmpDisable(VibeUInt8 nActuatorIndex)
 {
     int cnt = 0;
     unsigned char I2C_data[1];
@@ -147,13 +147,10 @@ IMMVIBESPIAPI VibeStatus SYS_API__I2C__Write( _addr, _data)
     return VIBE_S_SUCCESS;
 }
 
-EXPORT_SYMBOL(ImmVibeSPI_ForceOut_AmpDisable);
-
-
 /*
 ** Called to enable amp (enable output force0)
 */
-/*IMMVIBESPIAPI*/ VibeStatus ImmVibeSPI_ForceOut_AmpEnable(VibeUInt8 nActuatorIndex)
+IMMVIBESPIAPI VibeStatus ImmVibeSPI_ForceOut_AmpEnable(VibeUInt8 nActuatorIndex)
 {
     int cnt = 0;	
     unsigned char I2C_data[1];
@@ -279,7 +276,6 @@ EXPORT_SYMBOL(ImmVibeSPI_ForceOut_AmpDisable);
 
     return VIBE_S_SUCCESS;
 }
-EXPORT_SYMBOL(ImmVibeSPI_ForceOut_AmpEnable);
 
 /*
 ** Called at initialization time to set PWM frequencies, disable amp, etc...
