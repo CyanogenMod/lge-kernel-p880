d643ba9d766877ca4c827723a7a299ab7d07f2b5
demetris <ierokipides.dem@gmail.com>
2014-10-05 03:17:44 +0300

    ARM: Remove Leds idle start/stop calls as they are correctly set to idle notifiers
    Change-Id: I5488358425bf907e5ce48268592671f8cb5b3475

--- arch/arm/kernel/process.c
+++ arch/arm/kernel/process.c
@@ -34,7 +34,6 @@
 #include <linux/console.h>
 
 #include <asm/cacheflush.h>
-#include <asm/leds.h>
 #include <asm/processor.h>
 #include <asm/system.h>
 #include <asm/thread_notify.h>
@@ -248,7 +247,6 @@ void cpu_idle(void)
 	while (1) {
 		tick_nohz_idle_enter();
 		rcu_idle_enter();
-		leds_event(led_idle_start);
 		while (!need_resched()) {
 #ifdef CONFIG_HOTPLUG_CPU
 			if (cpu_is_offline(smp_processor_id()))
@@ -279,7 +277,6 @@ void cpu_idle(void)
 			} else
 				local_irq_enable();
 		}
-		leds_event(led_idle_end);
 		rcu_idle_exit();
 		tick_nohz_idle_exit();
 		preempt_enable_no_resched();
