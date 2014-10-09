da2400807fedeb619baee78cd41a9a76081023f7
demetris <ierokipides.dem@gmail.com>
2014-10-09 22:41:41 +0300

    Revert " ARM: substitute arch_idle()"
    This reverts commit 26fe32467a9068393714232bbb2553c714c9dada.
    
    Revert "ARM: clean up idle handlers"
    
    This reverts commit 6b98c2101b7a40012599cc9e41868c6ca3b4eb8e.

--- arch/arm/kernel/process.c
+++ arch/arm/kernel/process.c
@@ -61,6 +61,8 @@ extern void setup_mm_for_reboot(void);
 
 static volatile int hlt_counter;
 
+#include <mach/system.h>
+
 #ifdef CONFIG_SMP
 void arch_trigger_all_cpu_backtrace(void)
 {
@@ -216,17 +218,13 @@ void cpu_idle_wait(void)
 EXPORT_SYMBOL_GPL(cpu_idle_wait);
 
 /*
- * This is our default idle handler.
+ * This is our default idle handler.  We need to disable
+ * interrupts here to ensure we don't miss a wakeup call.
  */
-
-void (*arm_pm_idle)(void);
-
 static void default_idle(void)
 {
-	if (arm_pm_idle)
-		arm_pm_idle();
-	else
-		cpu_do_idle();
+	if (!need_resched())
+		arch_idle();
 	local_irq_enable();
 }
 
@@ -253,10 +251,6 @@ void cpu_idle(void)
 				cpu_die();
 #endif
 
-			/*
-			 * We need to disable interrupts here
-			 * to ensure we don't miss a wakeup call.
-			 */
 			local_irq_disable();
 #ifdef CONFIG_PL310_ERRATA_769419
 			wmb();
@@ -264,18 +258,19 @@ void cpu_idle(void)
 			if (hlt_counter) {
 				local_irq_enable();
 				cpu_relax();
-			} else if (!need_resched()) {
+			} else {
 				stop_critical_timings();
 				if (cpuidle_idle_call())
 					pm_idle();
 				start_critical_timings();
 				/*
-				 * pm_idle functions must always
-				 * return with IRQs enabled.
+				 * This will eventually be removed - pm_idle
+				 * functions should always return with IRQs
+				 * enabled.
 				 */
 				WARN_ON(irqs_disabled());
-			} else
 				local_irq_enable();
+			}
 		}
 		rcu_idle_exit();
 		tick_nohz_idle_exit();
