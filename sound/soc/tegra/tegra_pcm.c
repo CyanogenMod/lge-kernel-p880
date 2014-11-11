b9384da732369f42099f8ac13377adf56cbda89c
demetris <ierokipides.dem@gmail.com>
2014-11-11 02:48:50 +0200

    sound/soc/tegra/tegra_pcm: Add a INT_DURATION_THRESHOLD
    Change-Id: Ia5a20b64bc263027ce9f6f7e387d53616e840ba8

--- sound/soc/tegra/tegra_pcm.c
+++ sound/soc/tegra/tegra_pcm.c
@@ -44,6 +44,7 @@
 
 #define PERIOD_BYTES_MAX (PAGE_SIZE * 2)
 #define PERIODS_MAX 64
+#define INT_DURATION_THRESHOLD 32
 
 static const struct snd_pcm_hardware tegra_pcm_hardware = {
 	.info			= SNDRV_PCM_INFO_MMAP |
@@ -93,11 +94,15 @@ static void tegra_pcm_queue_dma(struct tegra_runtime_data *prtd)
 	tegra_dma_enqueue_req(prtd->dma_chan, dma_req);
 }
 
+static int64_t oldT1,TimeOutCounter=0;
 static void dma_complete_callback(struct tegra_dma_req *req)
 {
 	struct tegra_runtime_data *prtd = (struct tegra_runtime_data *)req->dev;
 	struct snd_pcm_substream *substream = prtd->substream;
 	struct snd_pcm_runtime *runtime = substream->runtime;
+
+	static int64_t IRQT1,T2;
+	IRQT1 = ktime_to_ms(ktime_get());
 
 	spin_lock(&prtd->lock);
 
@@ -112,6 +117,13 @@ static void dma_complete_callback(struct tegra_dma_req *req)
 	tegra_pcm_queue_dma(prtd);
 
 	spin_unlock(&prtd->lock);
+	T2 = IRQT1 - oldT1;
+	if (T2 > INT_DURATION_THRESHOLD)
+	{
+		TimeOutCounter++;
+		printk(KERN_INFO "[AUD] Dur = %llu, CNT = %llu", T2, TimeOutCounter);
+	}
+	oldT1 = IRQT1;
 
 	snd_pcm_period_elapsed(substream);
 }
@@ -236,7 +248,7 @@ int tegra_pcm_close(struct snd_pcm_substream *substream)
 		tegra_dma_free_channel(prtd->dma_chan);
 
 	kfree(prtd);
-
+	TimeOutCounter = 0;
 	return 0;
 }
 
@@ -296,6 +308,7 @@ int tegra_pcm_trigger(struct snd_pcm_substream *substream, int cmd)
 		prtd->dma_pos_end = frames_to_bytes(runtime, runtime->periods * runtime->period_size);
 		prtd->period_index = 0;
 		prtd->dma_req_idx = 0;
+		oldT1 = ktime_to_ms(ktime_get());
 		if (prtd->disable_intr) {
 			prtd->dma_req_count = 1;
 			prtd->dma_req[0].complete = NULL;
