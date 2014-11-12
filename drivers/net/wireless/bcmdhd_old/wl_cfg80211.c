b527bf163120baffa07ae6ad17e68b812a7bebd5
Thomas Wendt <thoemy@gmx.net>
2014-11-12 23:20:34 +0100

    Merge tag 'android-5.0.0_r0.6' into cm-12.0
    Android 5.0.0 Release 0.6
    
    Change-Id: I8db7d0f1d9cf73caee3d36423a79405b969429af

--- drivers/net/wireless/bcmdhd_old/wl_cfg80211.c
+++ drivers/net/wireless/bcmdhd_old/wl_cfg80211.c
@@ -4639,8 +4639,11 @@ static s32 wl_setup_wiphy(struct wireless_dev *wdev, struct device *sdiofunc_dev
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(3, 2, 0)
 	wdev->wiphy->flags |= WIPHY_FLAG_SUPPORTS_FW_ROAM;
 #endif
-
-#ifdef ENABLE_CUSTOM_REGULATORY_DOMAIN
+	/* AP_SME flag can be advertised to remove patch from wpa_supplicant */
+	wdev->wiphy->flags |= WIPHY_FLAG_HAVE_AP_SME;
+#if defined(CONFIG_PM)
+	wdev->wiphy->wowlan.flags = WIPHY_WOWLAN_ANY;
+#endif
 	WL_DBG(("Registering custom regulatory)\n"));
 	wdev->wiphy->flags |= WIPHY_FLAG_CUSTOM_REGULATORY;
 	wiphy_apply_custom_regulatory(wdev->wiphy, &brcm_regdom);
